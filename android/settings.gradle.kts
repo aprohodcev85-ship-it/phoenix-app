6632d051679dca46c3d0a5d53ede980e0a82ad22
aprohodcev85-ship-it <aprohodcev85@gmail.com>
2026-03-19 14:52:05 +0300

    Fix Flutter SDK path in Gradle settings

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,3 +1,20 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+// Находим путь к Flutter SDK из local.properties
+val flutterSdkPath = run {
+    val localProperties = Properties()
+    val localPropertiesFile = rootProject.file("local.properties")
+    if (localPropertiesFile.exists()) {
+        localProperties.load(FileInputStream(localPropertiesFile))
+    }
+    val sdkPath = localProperties.getProperty("flutter.sdk")
+    sdkPath ?: throw GradleException("flutter.sdk not found in local.properties")
+}
+
+// Подключаем Gradle плагины из Flutter SDK
+includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
 pluginManagement {
     repositories {
         google()
@@ -8,4 +25,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
+
+include(":app")
