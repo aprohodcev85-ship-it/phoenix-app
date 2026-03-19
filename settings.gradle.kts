ac2f8621abd708901566097d5fe58f684c7c5cdd
aprohodcev85-ship-it <aprohodcev85@gmail.com>
2026-03-19 14:33:54 +0300

    Force add root settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement { 
+    repositories { 
+        google() 
+        mavenCentral() 
+        gradlePluginPortal() 
+    } 
+} 
+ 
+plugins { 
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" 
+} 
+ 
+include(":app") 
