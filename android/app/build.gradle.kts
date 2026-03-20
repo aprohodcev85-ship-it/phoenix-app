723a54fffca7d5ca743c002c2b99606a3d1a3593
aprohodcev85-ship-it <aprohodcev85@gmail.com>
2026-03-20 16:48:40 +0300

    Add Gradle wrapper files and fix build configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
 
     kotlinOptions {
