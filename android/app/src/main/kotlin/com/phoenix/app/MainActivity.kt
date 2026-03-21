fd7a3d7c4f389c7594f635495874e4edecd7ebb3
aprohodcev85-ship-it <aprohodcev85@gmail.com>
2026-03-21 09:31:53 +0300

    Fix AndroidManifest package and MainActivity

---
+++ android/app/src/main/kotlin/com/phoenix/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.phoenix.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/phoenix/phoenix/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.phoenix.phoenix
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
